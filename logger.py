0c319e1c05d3e126809b1e775c750cbcaef97bde
Honeycut <c@spinnenkop.nl>
2024-03-26 10:05:00 +0100

    V1.4 - Functional WebUI
    Video demo coming soon.

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ from markupsafe import Markup
 from threading import Thread
 from telethon import TelegramClient, events
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 # Load environment variables from .env file
@@ -25,6 +26,9 @@ client = TelegramClient('session_name', API_ID, API_HASH)
 
 # Initialize Flask
 app = Flask(__name__, template_folder='.')
+
+# Dictionary to store the number of media files for each chat
+media_counts = {}
 
 @app.route('/')
 def index():
@@ -68,12 +72,26 @@ async def handle_incoming(event):
     
     # Log the message
     with open(log_filename, "a") as file:
+        # Format the message's datetime
+        message_time = message.date.astimezone().strftime('%Y-%m-%d %H:%M:%S')
+
         if message.media:
-            media = await message.download_media(file=media_dir)
-            media_path = os.path.join('media', str(chat_id), os.path.basename(media))
-            file.write(f"<p><b><u>{sender_id}</u></b>: {message.message} <a href='{media_path}'>View Media</a></p>\n")
-        else:
-            file.write(f"<p><b><u>{sender_id}</u></b>: {message.message}</p>\n")
+            # Increment the media count for this chat
+            media_counts[chat_id] = media_counts.get(chat_id, 0) + 1
             
+            # Use the media count as the filename
+            media_filename = f"{media_counts[chat_id]}{message.file.ext}"
+            media = await message.download_media(file=os.path.join(media_dir, media_filename))
+            
+            media_path = os.path.join('/', MEDIA_DIRECTORY, str(chat_id), media_filename)
+            
+            # Check the file extension to determine whether to use an <img> or <video> tag
+            if media_filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.tiff', '.svg', '.ico')):
+                file.write(f"<p><b><u>{message_time} {sender_id}</u></b>: {message.message} <img src='{media_path}' alt='Media' style='max-width: 250px; max-height: 250px;'> <a href='{media_path}' target='_blank'>[Direct link]</a></p>\n")
+            elif media_filename.lower().endswith(('.mp4', '.webm', '.ogg', '.mov', '.avi', '.mkv', '.flv', '.wmv', '.3gp', '.3g2', '.m4v', '.mpg', '.mpeg', '.m2v', '.m4v', '.f4v', '.f4p', '.f4a', '.f4b', '.mp4v', '.flv', '.vob')):
+                file.write(f"<p><b><u>{message_time} {sender_id}</u></b>: {message.message} <video src='{media_path}' controls style='max-width: 500px; max-height: 500px;'></video> <a href='{media_path}' target='_blank'>[Direct link]</a></p>\n")
+        else:
+            file.write(f"<p><b><u>{message_time} {sender_id}</u></b>: {message.message}</p>\n")
+
 if __name__ == '__main__':
     client.loop.run_until_complete(main())
